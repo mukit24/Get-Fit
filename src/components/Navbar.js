d82af7c0975afc55ac1f2f2037d868b8d59e4fb5
mukit24 <mukithasanpranto4@gmail.com>
2022-08-14 17:17:45 +0600

    set_up_and_navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Logo from '../assets/images/main_logo.png';
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <Link to='/' className="navbar-brand" href="#">
+                    <img src={Logo} alt="gym_logo" height="34" />
+                    <span className='px-2 fw-bold'>Get Fit</span>
+                </Link>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav m-auto mb-2 mb-lg-0">
+                        <li className="nav-item ">
+                            <Link to='/' className="nav-link active">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#exercise">Exercise</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <h2>ghdgsah</h2>
+    )
+}
+
+export default Home
